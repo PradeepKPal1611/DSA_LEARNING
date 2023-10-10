f58f9b528ffac6321de3a9a4957be46a264e84c5
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-11 00:02:19 +0530

    DSA PRACTICE

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
-
+import React, {useEffect, useState} from 'react';
 function App() {
+  const [name,setName]=useState("");
+  const [email,setEmail]=useState("");
+  const [mobile,setMobile]=useState("");
+function saveUser(){
+  console.log(name,email,mobile);
+  let Arr = {name, email, mobile}
+  fetch("http://localhost:4000/todo",{
+    method: 'POST', 
+    headers: {
+      'Accept':'application/json',
+      'Content-Type':'application/json'
+    },
+    body:JSON.stringify(Arr)
+  }).then((result)=>{
+    //console.log("result");
+    result.json().then((resp)=>{
+     console.lpg("resp",resp); 
+    })
+  })
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>POST API EXAMPLE</h1>
+      <input type='text' value={name} onChange={(e)=>{setName(e.target.value
+        )}} name='name'/><br/> <br/>
+      <input type='text' value={email} onChange={(e)=>{setEmail(e.target.value
+        )}} name='email'/><br/> <br/>
+      <input type='text' value={mobile} onChange={(e)=>{setMobile(e.target.value
+        )}} name='mobile'/><br/> <br/>
+      <button type='button'>Save New User</button>
     </div>
   );
 }
