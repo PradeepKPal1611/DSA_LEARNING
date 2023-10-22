03ea74a2a9420fbd557396823bad823df4215a1a
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-10-22 23:59:33 +0530

    6.IncrzDecrzLengthArray

---
+++ DSA_PRACTICE/JavaScript-Arrays/5.ConvertArrayToString.js
@@ -0,0 +1,6 @@
+// Convert an Array to String
+// We have an in-built method toString() in Javascript
+// that converts an array to a string.
+
+const courses = ["HTML", "CSS", "JavaScript"];
+console.log(courses.toString());
