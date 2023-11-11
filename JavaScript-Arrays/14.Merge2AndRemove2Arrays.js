d44b11de20ebca203843cf069666e74ff8fa71d9
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-11 23:29:29 +0530

    JavaScript-Arrays/14.Merge2AndRemove2Arrays.js

---
+++ JavaScript-Arrays/14.Merge2AndRemove2Arrays.js
@@ -0,0 +1,34 @@
+/*  <-- How to merge two arrays and remove duplicate items in 
+JavaScript ?
+
+In this article, we will see how to merge two arrays and 
+remove duplicate items from the merged array in JavaScript. 
+While working with the arrays in Javascript, we often used 
+to merge and also remove the duplication of items from the new array.
+
+There are some methods to merge two arrays and remove 
+duplicate items, which are given below: 
+
+Using Spread Operator and Set() Object
+Using concat() Method and Set() Object
+Using concat() Method and Filter() Method --> */
+
+/* <-- Method 1: Using Spread Operator and Set() 
+Object
+The Spread Operator is used to merge two arrays and 
+then use the Set() object to remove the duplicate items 
+from the merged array.
+
+Example:  --> */
+
+let arr1 = [1, 2, 3, 4, 5, 6];  
+let arr2 = [3, 4, 5, 7]; 
+let arr = [...arr1, ...arr2]; 
+let mergedArr = [...new Set(arr)] 
+console.log(mergedArr);
+
+/* Output
+[
+  1, 2, 3, 4,
+  5, 6, 7
+] */
