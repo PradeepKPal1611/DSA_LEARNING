c4a0eac7e6b867ea1e8157052680ecb3a8c13496
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-16 23:16:49 +0530

    Modified: JavaScript-Arrays/16.LinearSearchAlgoArray.js

--- JavaScript-Arrays/16.LinearSearchAlgoArray.js
+++ JavaScript-Arrays/16.LinearSearchAlgoArray.js
@@ -32,3 +32,31 @@ let num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
  /* Output
 8 is present in the array. */   
 
+/* <== Time complexity: O(n)
+The time complexity for this algorithm is O(n) as we are 
+looping through the array once to check for the given 
+element.
+
+Space complexity: O(1)
+
+The space complexity for this algorithm is O(1) as we are 
+not using any additional space other than the input array.
+ ==> */
+
+ /* <== 2. Using indexOf() function:
+
+The indexOf() function returns the index of the target element 
+in the array if it is present and -1 if not present.
+
+For example, 41 is not part of the num array in the code 
+below. ==> */
+
+let num1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let element1 = 41;
+if (num1.indexOf(element1) > 0)
+    console.log(element1 + " is present.");
+else
+    console.log(element1 + " is not present.");
+
+/* Output
+41 is not present. */
