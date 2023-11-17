0625984c0b87674746b9cdcf780e3b481f809c48
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-18 01:06:20 +0530

    Modified : JavaScript-Arrays/18.TwoArray.js

--- JavaScript-Arrays/18.TwoArray.js
+++ JavaScript-Arrays/18.TwoArray.js
@@ -57,3 +57,30 @@ console.log(convertToObj(a, b));
 { '1': 'ram', '2': 'shyam', '3': 'sita', '4': 'gita' } */
 
 
+/* <== Method 2: Using Object.assign method
+The Object.assign() method is used to copy the values and 
+properties from one or more source objects to a target object.
+
+Example: In this example, we will be using Javascript`s 
+Object.assign method. ==> */
+
+let a1 = [1, 2, 3, 4];
+let b2 = ["ram", "shyam", "sita", "gita"]
+ 
+// Checking if the array lengths are same 
+// and none of the array is empty
+function convertToObj(a1, b1) {
+    if (a1.length != b1.length || 
+        a1.length == 0 || 
+        b1.length == 0) {
+        return null;
+    }
+ 
+    // Using Object.assign method
+    return Object.assign(...a1.map((k, i) =>({ 
+                          [k]: b1[i] })))
+}
+console.log(convertToObj(a1, b1));
+
+/* Output
+{ '1': 'ram', '2': 'shyam', '3': 'sita', '4': 'gita' } */
