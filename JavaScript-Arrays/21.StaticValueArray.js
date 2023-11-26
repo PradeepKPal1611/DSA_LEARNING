ffec0e60843ace2d9f772f7826d9bb8606e43248
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-26 23:15:18 +0530

    Modified : JavaScript-Arrays/21.StaticValueArray.js

--- JavaScript-Arrays/21.StaticValueArray.js
+++ JavaScript-Arrays/21.StaticValueArray.js
@@ -36,7 +36,7 @@ let value = [4, 10, 5, 7, 6, 18];
  
 // Whole array is filled with 50 
 // and return the modified array 
-let value2 = value.fill(50);
+let value1 = value.fill(50);
 console.log(value);
 console.log(value2);
 
@@ -49,6 +49,23 @@ console.log(value2);
 fill() method to fill the array with 45 from the start 
 index is 1 to the end index is 4 (exclusive). ==> */
 
-let value1 = [4, 10, 5, 7, 6, 18];
-value1.fill(45, 1, 4);
-console.log(value1);
+let value2 = [4, 10, 5, 7, 6, 18];
+value2.fill(45, 1, 4);
+console.log(value2);
+
+/* Output:
+
+[ 4, 45, 45, 45, 6, 18 ] */
+
+/* <== Example 3: In the following example, we use the 
+fill() method to fill the array with 15. The start index 
+is 3 but the end index is not defined, so by default end 
+index is arr.length-1. ==> */
+
+let value3 = [4, 10, 5, 7, 6, 18];
+value3.fill(15, 3); 
+console.log(value3);
+
+/* Output:
+
+[ 4, 10, 5, 15, 15, 15 ] */
