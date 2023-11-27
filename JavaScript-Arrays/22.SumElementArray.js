216e20c25a3ac58a3ece50aee2b4d8731fe2475b
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-27 23:50:20 +0530

    JavaScript-Arrays/22.SumElementArray.js

---
+++ JavaScript-Arrays/22.SumElementArray.js
@@ -0,0 +1,36 @@
+/* <== How to find the sum of all elements of a given array
+ in JavaScript ?
+
+In this article, we will learn how we can find the sum of 
+all elements/numbers of the given array. There are many 
+approaches to solving the problems using for loop, 
+forEach() method, and reduce() method.
+
+Below all the approaches are described with a proper 
+example:
+
+Using for loop
+Using forEach() Method
+Using forEach() Method
+Using Recursion
+Method 1: Using for loop
+We are simply going to iterate over all the elements of 
+the array using a Javascript for loop to find the sum.
+
+Example: This example shows the above-explained approach. ==> */
+
+// Creating array
+let arr = [4, 8, 7, 13, 12]
+ 
+// Creating variable to store the sum
+let sum = 0;
+ 
+// Running the for loop
+for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+}
+ 
+console.log("Sum is " + sum) // Prints: 44
+
+/* Output
+Sum is 44 */
