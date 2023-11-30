fabdf1d7f0ac47778bf8a061157fabcaa278896e
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-11-30 23:38:41 +0530

    Modified : JavaScript-Arrays/25.removeNElementArray.js

--- JavaScript-Arrays/25.removeNElementArray.js
+++ JavaScript-Arrays/25.removeNElementArray.js
@@ -46,3 +46,41 @@ Original Array: [
   6, 7, 8, 9
 ]
 Modified Array: [ 1, 2, 3, 4, 5, 6 ] */
+
+/* <== Method 2: Using pop() Method
+It is used to remove the last element from the array. 
+This can be repeated in a loop of n iterations to remove 
+the last n elements of the array using the while loop.
+
+Syntax:
+
+array.pop();
+
+Example: In this example, we will be using the Javascript 
+pop() method to remove the elements from the end of a 
+given array. ==> */
+
+// Define the array
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+ 
+console.log("Original Array:", arr1);
+ 
+// Define the number of elements to remove
+let elemsToDelete1 = 5;
+ 
+// Loop for the number of elements
+// to delete
+while (elemsToDelete1--)
+ 
+    // Pop the last element from the
+    // end of the array
+    arr1.pop();
+ 
+console.log("Modified Array:", arr1);
+
+/* Output
+Original Array: [
+  1, 2, 3, 4, 5,
+  6, 7, 8, 9
+]
+Modified Array: [ 1, 2, 3, 4 ] */
