b528020dd2af89160cf498d0f61bbcce3e30cae0
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-12-16 23:30:40 +0530

    Modified : JavaScript-String/ CreateString.js

--- JavaScript-String/StringMethods.js
+++ JavaScript-String/StringMethods.js
@@ -23,3 +23,33 @@ console.log(str2);
 
 Welcome to GeesforGeeks
 [String: 'Welcome Geeks'] */
+
+/* <== JavaScript String Methods and Property
+String methods and properties are important to perform any 
+operation on the given string, String indexes start from 0. 
+The first character is in position 0 and the second in 1 
+and the same follows. We can call any of the pre-defined 
+methods of JavaScript as it automatically converts from 
+string primitive to objects.
+
+Example:
+
+ let gfg= "geeksforgeeks"
+Property:
+JavaScript String length Property
+This property returns the number of characters present in 
+the string. In the case of an array, this property returns 
+the number of elements present in the array.
+
+Example: This example describes the JavaScript String Length 
+property. ==> */
+
+// JavaScript to illustrate length property    
+function func() {
+    // length property for string
+    console.log("GFG".length)
+}
+func();
+
+/* Output
+3 */
