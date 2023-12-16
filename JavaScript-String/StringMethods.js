ddae98b6898fd43497c4f0fe63c9df9e60ae6651
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-12-16 23:28:52 +0530

    Modified : JavaScript-String/ CreateString.js

---
+++ JavaScript-String/StringMethods.js
@@ -0,0 +1,25 @@
+/* <== JavaScript String Methods
+
+JavaScript strings are the sequence of characters. In JavaScript, 
+strings automatically convert the string to objects so 
+that we can use string methods and properties also for 
+primitive strings. It is used to represent and manipulate 
+the sequence of characters.
+
+In general, there are two ways to create a string in JavaScript. 
+Either by using the new String() syntax or putting the 
+value in quotes.
+
+Example: This example, creates a string in JavaScript 
+using the methods described above. ==> */
+
+let str1 = "Welcome to GeesforGeeks";
+let str2 = new String("Welcome Geeks");
+ 
+console.log(str1);
+console.log(str2);
+
+/* Output
+
+Welcome to GeesforGeeks
+[String: 'Welcome Geeks'] */
