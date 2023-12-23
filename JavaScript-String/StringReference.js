273f1ef4d6532ed5cbedbd7e88e9a9c4e3f99bb5
PradeepKPal1611 <pradeepkumaracme16@gmail.com>
2023-12-23 23:54:07 +0530

    JavaScript-String/StringReference.js

---
+++ JavaScript-String/StringReference.js
@@ -0,0 +1,25 @@
+/* <== JavaScript String Reference
+
+In this article, we will learn JavaScript Strings and their 
+properties and methods with a brief description. JavaScript 
+Strings are used for storing and manipulating text content. 
+It can contain zero or more characters within single or double 
+quotes, like “Geeksforgeeks” or ‘Geeksforgeeks’.
+
+Syntax:
+
+String(object)
+
+Example: In this example, we will return the length of a 
+string. ==> */
+
+function stringLength() {
+    const str = "GeeksforGeeks";
+ 
+    console.log("String Length: " + str.length);
+}
+ 
+stringLength();
+
+/* Output:
+String Length: 13 */
